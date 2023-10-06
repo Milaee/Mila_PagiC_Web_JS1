f12742f4c810930f566b3b036266770791200f01
Milaee <milasahid23@gmail.com>
2023-10-06 19:31:03 +0700

    JS1

---
+++ script.js
@@ -0,0 +1,21 @@
+// switch case
+let angkaHari = 4 
+switch (angkaHari) {
+    case 1:
+        console.log("Hari ini adalah Senin")
+        break;
+    case 2:
+        console.log("Hari ini adalah Selasa")
+        break;
+    case 3:
+        console.log("Hari ini adalah Rabu")
+        break;
+    case 4:
+        console.log("Hari ini adalah Kamis")
+        break;
+    case 5:
+        console.log("Hari ini adalah Jumat")
+        break;
+        default:
+            console.log("Hari ini adalah Sabtu")
+}
